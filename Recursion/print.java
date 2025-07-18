bbe87fcbf12c25bd983345119c5e0fd0507aeb8f
priyanka1754 <priyankalbrce22@gmail.com>
2025-07-18 20:43:00 +0530

    Create print.java

---
+++ Recursion/print.java
@@ -0,0 +1,14 @@
+https://www.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1
+
+Print 1 To N Without Loop
+
+class Solution {
+    public void printNos(int n) {
+        // Code here
+        if(n==0){
+            return;
+        }
+        printNos(n-1);
+        System.out.print(n+" ");
+    }
+}
