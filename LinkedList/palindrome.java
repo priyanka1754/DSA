928707925317474a3b7ec4db5e8b8f2b16a0a54f
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-08-25 15:38:26 +0530

    Update palindrome.java

--- LinkedList/palindrome.java
+++ LinkedList/palindrome.java
@@ -27,3 +27,49 @@ class Solution {
         return true;
     }
 }
+
+
+//Another Method without using extra space..
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    public ListNode reverseList(ListNode head){
+        ListNode rev=null;
+        while(head!=null){
+            ListNode temp=head.next;
+            head.next=rev;
+            rev=head;
+            head=temp;
+        }
+        return rev;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null) return true;
+
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        ListNode secondHalf=reverseList(slow);
+        ListNode firstHalf=head;
+        while(secondHalf!=null){
+            if(firstHalf.val!=secondHalf.val) return false;
+            firstHalf=firstHalf.next;
+            secondHalf=secondHalf.next;
+        }
+        return true;
+    }
+}
