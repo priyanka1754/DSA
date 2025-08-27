b9e028754d7dddcce4b811c9440d49ebe8905508
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-08-27 22:48:34 +0530

    Create SameTree.java

---
+++ Trees/SameTree.java
@@ -0,0 +1,38 @@
+https://leetcode.com/problems/same-tree/
+
+100. Same Tree
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private void postorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            list.add(null); 
+            return;
+        }
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        List<Integer> plist = new ArrayList<>();
+        List<Integer> qlist = new ArrayList<>();
+        postorder(p, plist);
+        postorder(q, qlist);
+        return plist.equals(qlist);  
+    }
+}
