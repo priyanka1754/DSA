91e419244b649e63ee408f19054ffb89159fa088
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-08-27 16:26:23 +0530

    Update preOrder.java

--- Trees/preOrder.java
+++ Trees/preOrder.java
@@ -27,3 +27,36 @@ class Solution {
         return ans;
     }
 }
+
+
+//iterative method
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        if(root==null) return ans;
+        Stack<TreeNode> s=new Stack<>();
+        s.push(root);
+        while(!s.isEmpty()){
+            TreeNode cur=s.pop();
+            ans.add(cur.val);
+            if(cur.right!=null) s.push(cur.right);
+            if(cur.left!=null) s.push(cur.left);
+        }
+        return ans;
+    }
+}
