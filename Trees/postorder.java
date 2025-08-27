faf7c5ae809a9b05f6e2f349e0b571a70f6babf4
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-08-27 18:51:12 +0530

    Update postorder.java

--- Trees/postorder.java
+++ Trees/postorder.java
@@ -27,3 +27,46 @@ class Solution {
         return ans;
     }
 }
+
+
+//iterative method
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root);
+
+        while (!stack1.isEmpty()) {
+            TreeNode node = stack1.pop();
+            stack2.push(node);
+
+            if (node.left != null) stack1.push(node.left);
+            if (node.right != null) stack1.push(node.right);
+        }
+
+        while (!stack2.isEmpty()) {
+            ans.add(stack2.pop().val);
+        }
+        return ans;
+    }
+}
