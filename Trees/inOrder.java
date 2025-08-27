9e3ffda5c38ee420c61aa6b26acfb9a1b17f4e97
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-08-27 16:07:31 +0530

    Update inOrder.java

--- Trees/inOrder.java
+++ Trees/inOrder.java
@@ -28,3 +28,40 @@ class Solution {
         return ans;
     }
 }
+
+
+//iterative method..
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        Stack<TreeNode> s=new Stack<>();
+        TreeNode cur=root;
+        while(cur!=null || !s.isEmpty()){
+            while(cur!=null){
+                s.push(cur);
+                cur=cur.left;
+            }
+
+            cur=s.pop();
+            ans.add(cur.val);
+
+            cur=cur.right;
+        }
+        return ans;
+    }
+}
