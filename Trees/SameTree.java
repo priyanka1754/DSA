126986ba6eb3af68f755d6089f0088e4b78f32cb
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-08-27 22:56:25 +0530

    Update SameTree.java

--- Trees/SameTree.java
+++ Trees/SameTree.java
@@ -36,3 +36,14 @@ class Solution {
         return plist.equals(qlist);  
     }
 }
+
+
+//another method..
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val!=q.val) return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
+}
