40ec8dc015f9465ae58d0929f480a991d980b667
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-09-08 14:36:40 +0530

    Update pattern.java

--- cts/pattern.java
+++ cts/pattern.java
@@ -44,3 +44,34 @@ class Main {
         }
     }
 }
+
+
+
+
+
+
+//equi;ateral triangle..
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<n-i+1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
