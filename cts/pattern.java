8ba476c14ed3ae723f96b76dce9f8ceb15de4c6e
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-09-08 14:27:10 +0530

    Create pattern.java

---
+++ cts/pattern.java
@@ -0,0 +1,24 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        int n=3;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<n-i+1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+output:
+
+  *
+ **
+***
