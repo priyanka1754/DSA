81a64f1898735f85ad6026e57cc0676753cd867a
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-09-08 14:32:32 +0530

    Update pattern.java

--- cts/pattern.java
+++ cts/pattern.java
@@ -22,3 +22,25 @@ output:
   *
  **
 ***
+
+      or
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        // System.out.println("Try programiz.pro");
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==n || j==n || (i+j)>n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
