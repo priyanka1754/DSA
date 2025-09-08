872c489b5a864e8357806c2f333bcf8c44f9ffea
priyanka1754 <priyankalbrce22@gmail.com>
2025-09-08 15:40:30 +0530

    Create matrixDiagonal.java

---
+++ 2D-Arrays/matrixDiagonal.java
@@ -0,0 +1,17 @@
+https://leetcode.com/problems/matrix-diagonal-sum/
+
+
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        int n=mat.length;
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+            int j=n-i-1;
+            if(i!=j){
+                sum+=mat[i][j];
+            }
+        }
+        return sum;
+    }
+}
