cb582c914f6d99c658df437effc4e7b79f6c7c08
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-09-09 11:55:43 +0530

    Update mergeSortedArray.java

--- cts/mergeSortedArray.java
+++ cts/mergeSortedArray.java
@@ -15,3 +15,29 @@ class Solution {
         }
     }
 }
+
+
+//merge sort approch
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int a[]=new int[n+m];
+        int l1,l2,i;
+        for(l1=0,l2=0,i=0;l1<m && l2<n;i++){
+            if(nums1[l1]<nums2[l2]){
+                a[i]=nums1[l1++];
+            }else{
+                a[i]=nums2[l2++];
+            }
+        }
+        while(l1<m){
+            a[i++]=nums1[l1++];
+        }
+        while(l2<n){
+            a[i++]=nums2[l2++];
+        }
+        for(int j=0;j<(m+n);j++){
+            nums1[j]=a[j];
+        }
+    }
+}
