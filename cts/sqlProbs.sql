9d0a24edb388cb002520e1556d5c29b7f300ae0a
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-09-10 14:40:27 +0530

    Update sqlProbs.sql

--- cts/sqlProbs.sql
+++ cts/sqlProbs.sql
@@ -1,2 +1,5 @@
 https://leetcode.com/problems/rank-scores/
 select score,dense_rank() over(order by score desc) as "Rank" from scores
+
+https://leetcode.com/problems/second-highest-salary/
+select max(salary) SecondHighestSalary from Employee where salary<(select max(salary) from employee);
