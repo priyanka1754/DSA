379d5d9e51026cd8868652c4b8ab010dbb4d2be8
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-09-10 14:41:11 +0530

    Update sqlProbs.sql

--- cts/sqlProbs.sql
+++ cts/sqlProbs.sql
@@ -3,3 +3,6 @@ select score,dense_rank() over(order by score desc) as "Rank" from scores
 
 https://leetcode.com/problems/second-highest-salary/
 select max(salary) SecondHighestSalary from Employee where salary<(select max(salary) from employee);
+
+https://leetcode.com/problems/combine-two-tables/
+select p.firstName,p.lastName,a.city,a.state from Person p left join Address a on p.personId=a.personId 
