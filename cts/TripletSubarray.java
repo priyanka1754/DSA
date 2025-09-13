98f842c9b0c854e276c65966f9e82c5482fdf267
priyanka1754 <162479659+priyanka1754@users.noreply.github.com>
2025-09-13 15:50:56 +0530

    Update TripletSubarray.java

--- cts/TripletSubarray.java
+++ cts/TripletSubarray.java
@@ -29,6 +29,23 @@ class Main {
     }
 }
 
+//other approch to fing subArrayTriplets..
+public class SubarraySumTriplets {
+    public static int countTriplets(int[] arr, int N) {
+        int count = 0;
+        for (int i = 0; i <= N - 3; i++) {
+            if (arr[i] + arr[i + 2] == arr[i + 1]) {
+            count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 3, 5, 2, 4, 2};
+        int N = 8;
+        System.out.println(countTriplets(arr, N));
+    }
+}
 
 //if k is changing then, the above code will not work.
 import java.io.*;
