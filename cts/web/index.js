73677acffb74ddc7cf828db95fb1789371aec31b
priyanka1754 <priyankalbrce22@gmail.com>
2025-10-03 22:31:39 +0530

    changes

--- cts/web/index.js
+++ cts/web/index.js
@@ -7,3 +7,23 @@ document.getElementById("btn-add").addEventListener("click",()=>{
     li.textContent=`item ${count++}`;
     document.getElementById("list").appendChild(li);
 })
+
+document.getElementById("my-form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let name=document.getElementById("name").value.trim();
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("password").value;
+    let msg=document.getElementById("msg");
+    if(name && email.includes("@") && email.includes(".") && password.length>8){
+        msg.textContent=`form submitted with these details name:${name}, email:${email},password: ${password}`;
+        msg.style.color="green";
+    }else{
+        msg.textContent="smtg error";
+        msg.style.color="red";
+    }
+})
+
+document.getElementById("toggle").addEventListener("change",()=>{
+    const pwd=document.getElementById("password1");
+    pwd.type=this.checked?"text":"password";
+})
